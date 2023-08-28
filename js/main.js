0a9f4fecd961ba47e4abb328778b89d8e2206fbc
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-08-28 21:28:13 +0300

    Contact form linked with email address

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 } 
+
+document.getElementById("submit-button").addEventListener("click", function() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var subject = encodeURIComponent("Contact Form Submission from " + name);
+    var body = encodeURIComponent("Name: " + name + "\nEmail: " + email + "\n\nMessage:\n" + message);
+
+    var mailtoLink = "mailto:jerry.ekuwom@learninglions.org?subject=" + subject + "&body=" + body;
+
+    window.location.href = mailtoLink;
+});
